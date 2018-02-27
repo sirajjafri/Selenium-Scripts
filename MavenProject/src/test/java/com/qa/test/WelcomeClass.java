b4058648ac5b8998b24e9b198987c816c28b63e3
sirajjafri <sirajjafri22@gmail.com>
2018-02-27 08:04:38 +0530

    Second Commit

---
+++ MavenProject/src/test/java/com/qa/test/WelcomeClass.java
@@ -0,0 +1,10 @@
+package com.qa.test;
+import org.testng.annotations.Test;
+
+public class WelcomeClass {
+	@Test
+    public void first ()
+    {
+       System.out.println("Welcome to Maven test siraj");
+    }
+}
